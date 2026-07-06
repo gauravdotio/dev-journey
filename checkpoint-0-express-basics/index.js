5d9989891262d147905017dcb69bacb27352f69c
Gaurav Rawat <gauravrawatcr@gmail.com>
2026-07-06 22:21:23 +0530

    feat: add CRUD routes for notes

--- checkpoint-0-express-basics/index.js
+++ checkpoint-0-express-basics/index.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+let notes = [];
+
 app.get("/hello", (req, res) => {
   res.json({ message: "This is working!" });
 });
 
 app.get("/hi", (req, res) => {
   res.json({ message: "Hi is also working!" });
+});
+
+app.post("/notes", (req, res) => {
+  const newNOte = {
+    id: Date.now(),
+    text: req.body.text,
+  };
+  notes.push(newNOte);
+  res.status(201).json(newNOte);
+});
+
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.delete("/notes/:id", (req, res) => {
+  notes = notes.filter(note => note.id != req.params.id);
+  res.json({ success: true, message: "Notes deleted" });
 });
 
 app.listen(3000, () => {
